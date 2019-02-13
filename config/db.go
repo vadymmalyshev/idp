9dab693e96115b71a201708b21741ce571cf5919
Val <valentyn@holova.ch>
2019-02-13 16:27:34 +0700

    Revert "Revert "initial structure""
    This reverts commit 034bb7ca70ce118ac5c36737010b2832ebeb87bb.

---
+++ config/db.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"sync"
+
+	"github.com/jinzhu/gorm"
+)
+
+var dbOnce sync.Once
+var db *gorm.DB
+
+func initDatabase() {
+	var err error
+	db, err = gorm.Open("postgres", DBConn)
+
+	db.LogMode(true)
+
+	if err != nil {
+		logger.Panic("failed to init db:", err.Error())
+	}
+}
+
+func DB() *gorm.DB {
+	dbOnce.Do(initDatabase)
+
+	return db
+}
---
+++ pkg/errors/errors.go
@@ -0,0 +1,8 @@
+package errors
+
+import "errors"
+
+var (
+	ErrUserNotFound = errors.New("user wasn't found")
+	ErrNoChallenge  = errors.New("challenge code was'nt provided")
+)
